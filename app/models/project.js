610af228aaa15dd53c1831a8c7bec7ae6a0a57f4
trbmcginnis <taylormcgin@gmail.com>
2019-07-30 13:56:59 -0400

    remove unnecessary attributes

--- app/models/project.js
+++ app/models/project.js
@@ -25,28 +25,17 @@ export default class ProjectModel extends Model {
   // array of applicant objects
   @attr() applicants;
 
-  // semicolon delimited applicant names
-  @attr('string') dcp_projectid;
-
   @attr('string') dcp_name;
-
-  @attr('string') dcp_alterationmapnumber;
 
   @attr() dcp_applicanttype;
 
   @attr() dcp_borough;
-
-  @attr('string') dcp_bsanumber;
 
   @attr('string') dcp_ceqrnumber;
 
   @attr() dcp_ceqrtype;
 
   @attr('string') dcp_certifiedreferred;
-
-  @attr('string') dcp_decpermitnumber;
-
-  @attr() dcp_easeis;
 
   @attr('boolean') dcp_femafloodzonea;
 
@@ -56,14 +45,6 @@ export default class ProjectModel extends Model {
 
   @attr('boolean') dcp_femafloodzonev;
 
-  @attr() dcp_leaddivision;
-
-  @attr('string') dcp_lpcnumber;
-
-  @attr('string') dcp_nydospermitnumber;
-
-  @attr('boolean') dcp_previousactiononsite;
-
   @attr('string') dcp_projectbrief;
 
   @attr('string') dcp_projectname;
@@ -72,13 +53,7 @@ export default class ProjectModel extends Model {
 
   @attr() dcp_hiddenprojectmetrictarget;
 
-  @attr('boolean') dcp_sischoolseat;
-
-  @attr('boolean') dcp_sisubdivision;
-
   @attr('string') dcp_ulurp_nonulurp;
-
-  @attr('string') dcp_wrpnumber;
 
   @attr() dcp_communitydistrict;
 
@@ -90,9 +65,7 @@ export default class ProjectModel extends Model {
 
   @attr() bbls;
 
-  @attr({ defaultValue: () => EmptyFeatureCollection })
-  bbl_featurecollection
-
+  // do we need a milestones model?
   @attr() milestones;
 
   // array of action codes?
