7b47dabc31c164252d246670555e98c3f826c4b0
godfreyyeung <godfreyyeung@gmail.com>
2019-08-15 17:11:41 -0400

    participant-types component test

--- tests/integration/components/participant-types-test.js
+++ tests/integration/components/participant-types-test.js
@@ -2,25 +2,39 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+import seedMirage from '../../../mirage/scenarios/default';
 
 module('Integration | Component | participant-types', function(hooks) {
   setupRenderingTest(hooks);
+  setupMirage(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  hooks.beforeEach(async function() {
+    this.store = this.owner.lookup('service:store');
+    seedMirage(server);
+  });
 
-    await render(hbs`<ParticipantTypes />`);
+  test('it generates a list of participantTypes', async function(assert) {
+    // TODO: Retrieve by email?
+    this.user = await this.store.findRecord('user', 2, {
+      include: 'userProjectParticipantTypes.project,projects',
+    });
 
-    assert.equal(this.element.textContent.trim(), '');
+    this.project = await this.store.findRecord('project', 3);
 
-    // Template block usage:
+    // User 2, Project 3, has two recommendations because it has two UserProjectParticipantTypes:
+    // 'BB' and 'BP'
     await render(hbs`
-      <ParticipantTypes>
-        template block text
+      <ParticipantTypes
+        @project={{this.project}}
+        @user={{this.user}}
+      as |participantTypes|>
+        {{#each participantTypes as |partType|}}
+          {{partType}}.
+        {{/each}}
       </ParticipantTypes>
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.equal(this.element.textContent.replace(/ /g, '').replace(/(\r\n|\n|\r)/gm, '').trim(), 'BB.BP.');
   });
 });
